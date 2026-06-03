0532c4915c170d70c59cde6213c056fbdb8394f6
SadidRahman23 <iamsadidx@gmail.com>
2026-06-04 01:57:53 +0600

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -27,10 +27,7 @@ app.use((req, res, next) => {
 // ==========================================
 // 2. MONGODB CONNECTION & SCHEMAS
 // ==========================================
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
